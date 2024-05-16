38f4b967bcd9425f650678143aca90f0ccd0d267
NguyenGiangMinh <anhminhne14@gmail.com>
2024-05-16 22:01:59 +0700

    bai tap

---
+++ LTNC_08/InsertHead.cpp
@@ -0,0 +1,6 @@
+SinglyLinkedListNode* insertNodeAtHead(SinglyLinkedListNode* llist, int data) {
+    SinglyLinkedListNode *newNode = new SinglyLinkedListNode(data);
+    newNode->next = llist;
+    llist = newNode;
+    return llist;
+}
